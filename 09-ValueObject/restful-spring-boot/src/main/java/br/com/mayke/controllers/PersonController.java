426d1807669198ce900eda0b37e11c51a3eb6914
Mayke Ferreira <maykeferreiramonica@gmail.com>
2022-12-30 16:52:11 -0300

    Value Object and dozer mapper

---
+++ 09-ValueObject/restful-spring-boot/src/main/java/br/com/mayke/controllers/PersonController.java
@@ -0,0 +1,56 @@
+package br.com.mayke.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.mayke.data.vo.v1.PersonVO;
+import br.com.mayke.services.PersonServices;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+	@Autowired
+	private PersonServices service;
+
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO findById(@PathVariable(value = "id") Long id) {
+		return service.findById(id);
+	}
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<PersonVO> findAll() {
+		return service.findAll();
+	}
+	
+	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO create(@RequestBody PersonVO PersonVO) {
+		return service.create(PersonVO);
+	}
+	
+	@PutMapping(produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE)
+	public PersonVO update(@RequestBody PersonVO PersonVO) {
+		return service.update(PersonVO);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+
+}
