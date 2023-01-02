e03957594af4c35fc08030558409110ee8d04125
Mayke Ferreira <maykeferreiramonica@gmail.com>
2023-01-02 16:38:51 -0300

    Flyway migrations and customizing json serialization

---
+++ 12-CustomizingSerialization/restful-spring-boot/src/main/java/br/com/mayke/data/vo/v1/PersonVO.java
@@ -0,0 +1,77 @@
+package br.com.mayke.data.vo.v1;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id", "address", "first_name", "last_name", "gender"})
+public class PersonVO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	
+	@JsonProperty("first_name")
+	private String firstName;
+	
+	@JsonProperty("last_name")
+	private String lastName;
+	private String address;
+	
+	@JsonIgnore
+	private String gender;
+	
+	public PersonVO(Long id, String firstName, String lastName, String address, String gender) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.gender = gender;
+	}
+
+	public PersonVO() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	
+}
---
+++ 12-CustomizingSerialization/restful-spring-boot/src/main/java/br/com/mayke/services/PersonServices.java
@@ -0,0 +1,58 @@
+package br.com.mayke.services;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.mayke.data.vo.v1.PersonVO;
+import br.com.mayke.exceptions.ResourceNotFoundException;
+import br.com.mayke.mapper.DozerMapper;
+import br.com.mayke.model.Person;
+import br.com.mayke.repositories.PersonRepository;
+
+@Service
+public class PersonServices {
+	
+	private Logger logger = Logger.getLogger(PersonServices.class.getName());
+	
+	@Autowired
+	private PersonRepository repository;
+	
+	public PersonVO findById(Long id) {
+		var entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		return DozerMapper.parseObject(entity, PersonVO.class);
+	}
+	
+	public List<PersonVO> findAll() {
+		logger.info("Finding all people");
+		return DozerMapper.parseListObjects(repository.findAll(), PersonVO.class);
+	}
+	
+	public PersonVO create(PersonVO person) {
+		logger.info("Creating one person!");
+		var entity = DozerMapper.parseObject(person, Person.class);
+		var vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class); ;
+		return vo;
+	}
+	
+	public PersonVO update(PersonVO person) {
+		logger.info("Updating one person!");
+		var entity = repository.findById(person.getId()).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAddress(person.getAddress());
+		entity.setGender(person.getGender());
+		
+		var vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class); ;
+		return vo;
+	}
+	
+	public void delete(Long id) {
+		logger.info("Deleting one person!");
+		var entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		repository.delete(entity);
+	}
+
+}
